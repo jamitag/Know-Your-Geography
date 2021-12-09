d09f81793d51baec1919d8a3b68a8fde7a2ca438
jamitag <jamgra12@gmail.com>
2021-12-09 15:29:42 +0000

    Create additional html pages as required

---
+++ assets/js/script.js
@@ -0,0 +1,29 @@
+let firstQuestion  = {
+    question: 'In which UK city would you find the river Clyde?',
+    answers: ['Dundee', 'Glasgow', 'Manchester', 'Sheffield'],
+    correctAnswer: 1
+}
+
+let secondQuestion  = {
+    question: 'In which city would you  find La Sagrada Familia?',
+    answers: ['Berlin', 'Lisbon', 'Geneva', 'Barcelona'],
+    correctAnswer: 3
+}
+
+let thirdQuestion  = {
+    question: 'The Strait of Gibraltar separates the Iberian peninsular from which African nation?',
+    answers: ['Western Sahara', 'Morocco', 'Algeria', 'Tunisia'],
+    correctAnswer: 1
+}
+
+let fourthQuestion  = {
+    question: 'What is the only country with a coastline on both the Red Sea and the Persian Gulf?',
+    answers: ['Saudi Arabia', 'Yemen', 'Jordan', 'Iran'],
+    correctAnswer: 0
+}
+
+let fifthQuestion  = {
+    question: 'Sofia is the capital of which country?',
+    answers: ['Romania', 'Moldova', 'Bulgaria', 'Kosovo'],
+    correctAnswer: 2
+}
