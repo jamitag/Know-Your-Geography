94d0e11536d5fc2e972ac2ac25d144ade879436f
jamitag <jamgra12@gmail.com>
2021-12-10 10:32:44 +0000

    Add question variables

--- assets/js/script.js
+++ assets/js/script.js
@@ -27,3 +27,48 @@ let fifthQuestion  = {
     answers: ['Romania', 'Moldova', 'Bulgaria', 'Kosovo'],
     correctAnswer: 2
 }
+
+let sixthQuestion  = {
+    question: 'Azerbaijan borders on which sea?'
+    answers: ['Persian Gulf', 'Red Sea', 'Black Sea', 'Caspian Sea'],
+    correctAnswer: 3
+}
+
+let seventhQuestion  = {
+    question: 'Which is the only country that both the Equator and the Tropic of Capricorn pass?'
+    answers: ['Brazil', 'Kenya', 'Indonesia', 'Colombia'],
+    correctAnswer: 0
+}
+
+let eighthQuestion  = {
+    question: 'The world largest port can be found in which city?'
+    answers: ['Singapore', 'Dubai', 'Shanghai', 'New York'],
+    correctAnswer: 2
+}
+
+let ninthQuestion  = {
+    question: 'A road tunnel runs from Pelerins in France to Entreves in Italy under which mountain?'
+    answers: ['Monte Rosa', 'Mont Blanc', 'Lyskamm', 'Matterhorn'],
+    correctAnswer: 1
+}
+
+let tenthQuestion  = {
+    question: 'What is the Longest river in India?'
+    answers: ['Ganges', 'Narmada', 'Yamuna' , 'Indus'],
+    correctAnswer: 1
+}
+
+// set question
+
+function showQuestion(q) {
+    let selectQuestion = document.getElementById('question');
+    selectQuestion.textContent = q.question;
+    let answers = document.querySelectorAll('.answer-button');
+    answers.forEach(function(element, index){
+        element.textContent = q.answer-button[index]
+    })
+}
+
+showQuestion(firstQuestion);
+
+
