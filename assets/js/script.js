ec212924dd1b48d307e28978932c32e80f610122
jamitag <jamgra12@gmail.com>
2021-12-13 18:48:03 +0000

    edit changeAnswer function and create answerQuestion

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,81 +1,70 @@
-let array = [
+let questionsArray = [
     {
         question: "In which UK city would you find the river Clyde?",
         answer: ['Dundee', 'Glasgow', 'Manchester', 'Sheffield'],
         correctAnswer: "Glasgow"
+    }, 
+    {
+        question: "In which city would you find La Sagrada Familia?",
+        answer: ['Berlin', 'Lisbon', 'Geneva', 'Barcelona'],
+        correctAnswer: "Barcelona"
+    }, 
+    {
+        question: "The Strait of Gibraltar separates the Iberian peninsular from which African nation?",
+        answer: ['Western Sahara', 'Morocco', 'Algeria', 'Tunisia'],
+        correctAnswer: "Morocco"
+    }, 
+    {
+        question: "What is the only country with a coastline on both the Red Sea and the Persian Gulf?",
+        answer: ['Saudi Arabia', 'Yemen', 'Jordan', 'Iran'],
+        correctAnswer: "Saudi Arabia"
+    },
+    {
+        question: "Sofia is the capital of which country?",
+        answer: ['Romania', 'Moldova', 'Bulgaria', 'Kosovo'],
+        correctAnswer: "Bulgaria"
+    },
+    {
+        question: "Azerbaijan borders on which sea?",
+        answer: ['Persian Gulf', 'Red Sea', 'Black Sea', 'Caspian Sea'],
+        correctAnswer: "Caspian Sea"
+    },
+    {
+        question: "Which is the only country that both the Equator and the Tropic of Capricorn pass",
+        answer: ['Brazil', 'Kenya', 'Indonesia', 'Colombia'],
+        correctAnswer: "Brazil"
+    },
+    {
+        question: "The world largest port can be found in which city?",
+        answer: ['Singapore', 'Dubai', 'Shanghai', 'New York'],
+        correctAnswer: "Shanghai"
+    },
+    {
+        question: "A road tunnel runs from Pelerins in France to Entreves in Italy under which mountain?",
+        answer: ['Monte Rosa', 'Mont Blanc', 'Lyskamm', 'Matterhorn'],
+        correctAnswer: "Mont Blanc"
+    },
+    {
+        question: "What is the Longest river in India?",
+        answer: ['Ganges', 'Narmada', 'Yamuna' , 'Indus'],
+        correctAnswer: "Ganges"
     }
 ]
 
-
-
-
-
-// let firstQuestion  = {
-//     question: 'In which UK city would you find the river Clyde?',
-//     answers: ['Dundee', 'Glasgow', 'Manchester', 'Sheffield'],
-//     correctAnswer: 1
-// }
-
-// let secondQuestion  = {
-//     question: 'In which city would you  find La Sagrada Familia?',
-//     answers: ['Berlin', 'Lisbon', 'Geneva', 'Barcelona'],
-//     correctAnswer: 3
-// }
-
-// let thirdQuestion  = {
-//     question: 'The Strait of Gibraltar separates the Iberian peninsular from which African nation?',
-//     answers: ['Western Sahara', 'Morocco', 'Algeria', 'Tunisia'],
-//     correctAnswer: 1
-// }
-
-// let fourthQuestion  = {
-//     question: 'What is the only country with a coastline on both the Red Sea and the Persian Gulf?',
-//     answers: ['Saudi Arabia', 'Yemen', 'Jordan', 'Iran'],
-//     correctAnswer: 0
-// }
-
-// let fifthQuestion  = {
-//     question: 'Sofia is the capital of which country?',
-//     answers: ['Romania', 'Moldova', 'Bulgaria', 'Kosovo'],
-//     correctAnswer: 2
-// }
-
-// let sixthQuestion  = {
-//     question: 'Azerbaijan borders on which sea?'
-//     answers: ['Persian Gulf', 'Red Sea', 'Black Sea', 'Caspian Sea'],
-//     correctAnswer: 3
-// }
-
-// let seventhQuestion  = {
-//     question: 'Which is the only country that both the Equator and the Tropic of Capricorn pass?'
-//     answers: ['Brazil', 'Kenya', 'Indonesia', 'Colombia'],
-//     correctAnswer: 0
-// }
-
-// let eighthQuestion  = {
-//     question: 'The world largest port can be found in which city?'
-//     answers: ['Singapore', 'Dubai', 'Shanghai', 'New York'],
-//     correctAnswer: 2
-// }
-
-// let ninthQuestion  = {
-//     question: 'A road tunnel runs from Pelerins in France to Entreves in Italy under which mountain?'
-//     answers: ['Monte Rosa', 'Mont Blanc', 'Lyskamm', 'Matterhorn'],
-//     correctAnswer: 1
-// }
-
-// let tenthQuestion  = {
-//     question: 'What is the Longest river in India?'
-//     answers: ['Ganges', 'Narmada', 'Yamuna' , 'Indus'],
-//     correctAnswer: 1
-// }
-
-// set question
+// global variables
 
 let i = 0;
+let score = 0;
+let total = questionsArray.length;
+let answer = "";
+let buttons=document.querySelectorAll('[id^="button-"]')
+let scoreText = document.getElementById("score")
+scoreText.textContent=score;
+let totalText = document.getElementById("total");
+totalText.textContent=total;
 
 
-//when clicking the play button, the user will be taken to first question
+//this function will allow clicking of the play button to take the user to the first question
 function startQuiz() {
     let primaryBox = document.getElementById("primary-box");
     primaryBox.style.display = "inline";
@@ -84,17 +73,42 @@ function startQuiz() {
     nextQuestion();
 }
 
+//clicking the 'next' button will move onto the next question
 function nextQuestion() {
     let selectQuestion = document.getElementById('question');
-    selectQuestion.textContent = array[i].question;
-    let buttons=document.querySelectorAll('[id^="button-"]')
+    selectQuestion.textContent = questionsArray[i].question;
 
-    for (var x=0; x < buttons.length; x++) {
-        buttons[x].textContent = array[i].answer[x]
+    for (let x=0; x < buttons.length; x++) {
+        buttons[x].textContent = questionsArray[i].answer[x]
+        buttons[x].classList.remove("active")
     }
-
+    answer = "";
     i++;
 }
+
+//assign the chosen value for user to submit
+function changeAnswer(inputButton, input){
+    answer = input
+
+    for(var x=0; x < buttons.length; x++){
+        if(buttons[x].classList.contains("active")){
+            buttons[x].classList.remove("active")
+        }
+    }
+    inputButton.classList.add("active")
+}
+
+//identifying correct answer and adding to score then moving on to next question
+function answerQuestion() {
+    if(answer === ""){
+        return
+    }
+    if (answer === questionsArray[i].correctAnswer){
+        score++
+    }
+    nextQuestion()
+}
+
 
 
 
